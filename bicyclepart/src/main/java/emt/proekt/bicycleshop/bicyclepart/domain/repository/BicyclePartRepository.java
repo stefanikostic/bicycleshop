8a2a7c740db3061094a61e73757a65f41ef530a5
Stefani Kostic <stefanikostic6@gmail.com>
2020-07-10 23:33:18 +0200

    Delete BicyclePartRepository.java

--- bicyclepart/src/main/java/emt/proekt/bicycleshop/bicyclepart/domain/repository/BicyclePartRepository.java
+++
@@ -1,8 +0,0 @@
-package emt.proekt.bicycleshop.bicyclepart.domain.repository;
-
-import emt.proekt.bicycleshop.bicyclepart.domain.model.BicyclePart;
-import emt.proekt.bicycleshop.sharedkernel.domain.base.ProductId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BicyclePartRepository extends JpaRepository<BicyclePart, ProductId> {
-}
