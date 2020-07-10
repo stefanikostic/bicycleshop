1b96a7ac614ddb6420cfc55d3344e45b4b6f99d2
Stefani Kostic <stefanikostic6@gmail.com>
2020-07-10 23:30:35 +0200

    Delete BicycleRepository.java

--- bicycle/src/main/java/emt/proekt/bicycleshop/bicycle/domain/repository/BicycleRepository.java
+++
@@ -1,8 +0,0 @@
-package emt.proekt.bicycleshop.bicycle.domain.repository;
-
-import emt.proekt.bicycleshop.bicycle.domain.model.Bicycle;
-import emt.proekt.bicycleshop.sharedkernel.domain.base.ProductId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BicycleRepository extends JpaRepository<Bicycle, ProductId> {
-}
